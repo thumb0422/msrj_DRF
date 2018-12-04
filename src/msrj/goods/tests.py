efb006ea29aff6916bbcd5ce4e6e95165ddd6984
liuchunhua <thumb0422@163.com>
2018-12-04 22:26:21 +0800

    update

--- src/msrj/goods/tests.py
+++ src/msrj/goods/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from msrj.dbPool import session
+from msrj.goods.models import Game
 
-# Create your tests here.
+
+game1 = Game(
+    # company=nintendo,
+    category="ACT",
+    name="Super Mario Bros",
+    release_date='1985-10-18'
+)
+
+# session.add(game1)
+# session.commit()
+
+
+qrys = session.query(Game).filter_by(category="ACT")
+
+print(qrys.count())
