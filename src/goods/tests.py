ae00252e506271a1ff2eb274b7294eb60b395f80
liuchunhua1 <chunhua1@eastmoney.com>
2018-12-12 15:39:47 +0800

    update
    add testdata

--- src/goods/tests.py
+++ src/goods/tests.py
@@ -2,13 +2,13 @@ from msrj.dbPool import session
 from goods.models import ProductInfo
 import random
 
-product1 = ProductInfo(
-    code = 'A' + str(random.randint(100,200)),
-    name = '产品一',
-    costPrice = random.random()
-)
-
-session.add(product1)
+for num in range(1,10):
+    product1 = ProductInfo(
+        code = 'A' + str(num),
+        name = '产品' + str(num),
+        costPrice = random.random()
+    )
+    session.add(product1)
 session.commit()
 
 # prod1 = session.query(ProductInfo)
